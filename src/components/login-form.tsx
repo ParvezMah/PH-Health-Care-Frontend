a2194a56d43a4ac5e27c1c64c18b6ae95750d702
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2025-11-06 00:10:41 +0600

    66-6 Completing Login Form Handling

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -1,12 +1,17 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
+import { useActionState } from "react";
 import { Button } from "./ui/button";
 import { Field, FieldDescription, FieldGroup, FieldLabel } from "./ui/field";
 import { Input } from "./ui/input";
+import { loginUser } from "@/services/auth/loginUser";
 
 const LoginForm = () => {
+  const [state, formAction, isPending] = useActionState(loginUser, null);
+  console.log({state})
+
   return (
-    <form>
+    <form action={formAction}>
       <FieldGroup>
         <div className="grid grid-cols-1 gap-4">
           {/* Email */}
@@ -35,8 +40,8 @@ const LoginForm = () => {
         </div>
         <FieldGroup className="mt-4">
           <Field>
-            <Button type="submit">
-              {/* {isPending ? "Logging in..." : "Login"} */}
+              <Button type="submit" disabled={isPending}>
+              {isPending ? "Logging in..." : "Login"}
             </Button>
 
             <FieldDescription className="px-6 text-center">
