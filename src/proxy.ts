664fc9d0ecdc228a6685b0f174523675097d0852
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2025-11-08 14:32:16 +0600

    67-6 Protecting and Authorizing the pages with Main Proxy Function Part - 1

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,12 +1,62 @@
+import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { getDefaultDashboardRoute, getRouteOwner, isAuthRoute, UserRole } from "./lib/auth-utils";
+import jwt, { JwtPayload } from 'jsonwebtoken';
 
 // This function can be marked `async` if using `await` inside
-export function proxy(request: NextRequest) {
-  //   return NextResponse.redirect(new URL('/consultation', request.url))
-  const pathname = request.nextUrl.pathname;
-  console.log("Middleware pathname : ", pathname);
-  return NextResponse.next();
+export async function proxy(request: NextRequest) {
+    const cookieStore = await cookies()
+    const pathname = request.nextUrl.pathname;
+
+    const accessToken = request.cookies.get("accessToken")?.value || null;
+
+    let userRole: UserRole | null = null;
+    if (accessToken) {
+        const verifiedToken: JwtPayload | string = jwt.verify(accessToken, process.env.JWT_SECRET as string);
+
+        if (typeof verifiedToken === "string") {
+            cookieStore.delete("accessToken");
+            cookieStore.delete("refreshToken");
+            return NextResponse.redirect(new URL('/login', request.url));
+        }
+
+        userRole = verifiedToken.role;
+    }
+
+    const routerOwner = getRouteOwner(pathname);
+    //path = /doctor/appointments => "DOCTOR"
+    //path = /my-profile => "COMMON"
+    //path = /login => null
+
+    const isAuth = isAuthRoute(pathname)
+
+    // Rule 1 : User is logged in and trying to access auth route. Redirect to default dashboard
+    if (accessToken && isAuth) {
+        return NextResponse.redirect(new URL(getDefaultDashboardRoute(userRole as UserRole), request.url))
+    }
+
+    // Rule 2 : User is trying to access open public route
+    if (routerOwner === null) {
+        return NextResponse.next();
+    }
+
+    // Rule 1 & 2 for open public routes and auth routes
+
+    if (!accessToken) {
+        const loginUrl = new URL("/login", request.url);
+        loginUrl.searchParams.set("redirect", pathname);
+        return NextResponse.redirect(loginUrl);
+    }
+
+    // Rule 3 : User is trying to access common protected route
+    if (routerOwner === "COMMON") {
+        return NextResponse.next();
+    }
+
+
+    console.log("userRole in proxy middleware: ", userRole);
+    return NextResponse.next();
 }
 
 // See "Matching Paths" below to learn more
