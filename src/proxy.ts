36d4a1fcffe5b938aaf530c41a405b05256dfd72
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2025-11-08 12:53:16 +0600

    67-3 Planning On Route Protection & Authorization

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,12 +1,24 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
- 
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
 // This function can be marked `async` if using `await` inside
 export function proxy(request: NextRequest) {
-  return NextResponse.redirect(new URL('/dashboard', request.url))
+  //   return NextResponse.redirect(new URL('/consultation', request.url))
+  const pathname = request.nextUrl.pathname;
+  console.log("Middleware pathname : ", pathname);
+  return NextResponse.next();
 }
- 
+
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: '/',
-}
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|.well-known).*)",
+  ],
+};
