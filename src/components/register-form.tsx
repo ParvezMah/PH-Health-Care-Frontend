3f5e5f63719803d39c4236b609323fcf6ca5c13b
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2025-11-08 11:01:17 +0600

    66-8 Register Form Error Handling And Data Validations

--- src/components/register-form.tsx
+++ src/components/register-form.tsx
@@ -10,6 +10,20 @@ import { registerPatient } from "@/services/auth/registerPatient";
 const RegisterForm = () => {
   const [state, formAction, isPending] = useActionState(registerPatient, null);
   console.log("state : ", state, "isPending : ", isPending);
+
+  const getFieldError = (fieldName: string) => {
+    if (state && state.errors) {
+      const error = state.errors.find((err: any) => err.field === fieldName);
+      if (error) {
+        return error.message;
+      } else {
+        return null;
+      }
+    } else {
+      return null;
+    }
+  };
+
   return (
     <form action={formAction}>
       <FieldGroup>
@@ -18,6 +32,11 @@ const RegisterForm = () => {
           <Field>
             <FieldLabel htmlFor="name">Full Name</FieldLabel>
             <Input id="name" name="name" type="text" placeholder="John Doe" />
+            {getFieldError("name") && (
+              <FieldDescription className="text-red-600">
+                {getFieldError("name")}
+              </FieldDescription>
+            )}
           </Field>
           {/* Address */}
           <Field>
@@ -28,6 +47,12 @@ const RegisterForm = () => {
               type="text"
               placeholder="123 Main St"
             />
+
+            {getFieldError("address") && (
+              <FieldDescription className="text-red-600">
+                {getFieldError("address")}
+              </FieldDescription>
+            )}
           </Field>
           {/* Email */}
           <Field>
@@ -38,11 +63,21 @@ const RegisterForm = () => {
               type="email"
               placeholder="m@example.com"
             />
+            {getFieldError("email") && (
+              <FieldDescription className="text-red-600">
+                {getFieldError("email")}
+              </FieldDescription>
+            )}
           </Field>
           {/* Password */}
           <Field>
             <FieldLabel htmlFor="password">Password</FieldLabel>
             <Input id="password" name="password" type="password" />
+            {getFieldError("password") && (
+              <FieldDescription className="text-red-600">
+                {getFieldError("password")}
+              </FieldDescription>
+            )}
           </Field>
           {/* Confirm Password */}
           <Field className="md:col-span-2">
@@ -52,6 +87,11 @@ const RegisterForm = () => {
               name="confirmPassword"
               type="password"
             />
+            {getFieldError("confirmPassword") && (
+              <FieldDescription className="text-red-600">
+                {getFieldError("confirmPassword")}
+              </FieldDescription>
+            )}
           </Field>
         </div>
         <FieldGroup className="mt-4">
