3436ec62833144e1870ef86b4140a7add0553738
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2025-11-12 15:01:20 +0600

    68-7 Making DashboardNavbar Component

--- src/components/modules/Dashboard/DashboardNavbar.tsx
+++ src/components/modules/Dashboard/DashboardNavbar.tsx
@@ -1,8 +1,10 @@
+import { getUserInfo } from "@/services/auth/getUserInfo";
+import DashboardNavbarContent from "./DashboardNavbarContent";
+import { UserInfo } from "@/types/user.interface";
 
-const DashboardNavbar = () => {
-  return (
-    <div>DashboardNavbar</div>
-  )
-}
+const DashboardNavbar = async () => {
+  const userInfo = await getUserInfo() as UserInfo;
+  return <DashboardNavbarContent userInfo={userInfo} />;
+};
 
-export default DashboardNavbar
+export default DashboardNavbar;
