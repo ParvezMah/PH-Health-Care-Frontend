14acb1b5f1b6a8776394370c5be106ce2a8b15a0
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2025-11-12 16:15:28 +0600

    68-9 Making DashboardMobileSidebar Component

--- src/components/modules/Dashboard/DashboardNavbar.tsx
+++ src/components/modules/Dashboard/DashboardNavbar.tsx
@@ -1,10 +1,20 @@
 import { getUserInfo } from "@/services/auth/getUserInfo";
 import DashboardNavbarContent from "./DashboardNavbarContent";
 import { UserInfo } from "@/types/user.interface";
+import { getDefaultDashboardRoute } from "@/lib/auth-utils";
+import { NavSection } from "@/types/dashboard.interface";
 
 const DashboardNavbar = async () => {
   const userInfo = (await getUserInfo()) as UserInfo;
-  return <DashboardNavbarContent userInfo={userInfo} />;
+  const navItems: NavSection[] = [];
+  const dashboardHome = getDefaultDashboardRoute(userInfo.role);
+  return (
+    <DashboardNavbarContent
+      userInfo={userInfo}
+      navItems={navItems}
+      dashboardHome={dashboardHome}
+    />
+  );
 };
 
 export default DashboardNavbar;
