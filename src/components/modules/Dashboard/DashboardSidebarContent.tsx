914cdcb0dce4a335572206cd271f960a785fec0f
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2025-11-12 16:46:06 +0600

    68-11 Creating Icon Mapper Function To Get Dynamic Icons For Sidebar

--- src/components/modules/Dashboard/DashboardSidebarContent.tsx
+++ src/components/modules/Dashboard/DashboardSidebarContent.tsx
@@ -3,6 +3,7 @@
 import { Badge } from "@/components/ui/badge";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Separator } from "@/components/ui/separator";
+import { getIconComponent } from "@/lib/icon-mapper";
 import { cn } from "@/lib/utils";
 import { NavSection } from "@/types/dashboard.interface";
 import { UserInfo } from "@/types/user.interface";
@@ -45,8 +46,7 @@ const DashboardSidebarContent = ({
               <div className="space-y-1">
                 {section.items.map((item) => {
                   const isActive = pathname === item.href;
-                  //   const Icon = getIconComponent(item.icon);
-                  const Icon = <Bell />;
+                    const Icon = getIconComponent(item.icon);
 
                   return (
                     <Link
@@ -59,8 +59,7 @@ const DashboardSidebarContent = ({
                           : "text-muted-foreground hover:bg-accent hover:text-accent-foreground"
                       )}
                     >
-                      {/* <Icon className="h-4 w-4" /> */}
-                      <Bell className="h-4 w-4" />
+                      <Icon className="h-4 w-4" />
                       <span className="flex-1">{item.title}</span>
                       {item.badge && (
                         <Badge
