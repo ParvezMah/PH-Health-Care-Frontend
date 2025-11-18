4660309620e80c70b5c55c6f5e427f0a260d67c4
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2025-11-18 20:23:01 +0600

    70-2 Creating Page Header For Specialities Management Table

--- src/app/(dashboardLayout)/admin/dashboard/specialities-management/page.tsx
+++ src/app/(dashboardLayout)/admin/dashboard/specialities-management/page.tsx
@@ -1,5 +1,22 @@
-const AdminSchedulesManagementPage = () => {
-  return <div>AdminSchedulesManagementPage</div>;
+
+import SpecialitiesManagementHeader from "@/components/modules/Admin/SpecialitiesManagementHeader";
+import RefreshButton from "@/components/shared/RefreshButton";
+import { getSpecialities } from "@/services/admin/specialitiesManagement";
+
+const AdminSpecialitiesManagementPage = async () => {
+  const result = await getSpecialities();
+  return (
+    <div className="space-y-6">
+      <SpecialitiesManagementHeader />
+      <div className="flex">
+        <RefreshButton />
+      </div>
+
+      {/* <Suspense fallback={<TableSkeleton columns={2} rows={10} />}>
+        <SpecialitiesTable specialities={result.data} />
+      </Suspense> */}
+    </div>
+  );
 };
 
-export default AdminSchedulesManagementPage;
+export default AdminSpecialitiesManagementPage;
