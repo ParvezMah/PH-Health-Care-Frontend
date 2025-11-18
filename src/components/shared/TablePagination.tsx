5f7f740380b4238009cea2b50a1a5027c39a19f2
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2025-11-18 12:12:19 +0600

    69-7 Analyzing The Table Pagination Component

---
+++ src/components/shared/TablePagination.tsx
@@ -0,0 +1,13 @@
+
+const TablePagination = () => {
+  return (
+    <div>
+        <h1>TablePagination</h1>
+        <h1>69-7 Analyzing The Table Pagination Component</h1>
+    </div>
+  )
+}
+
+export default TablePagination
+
+
