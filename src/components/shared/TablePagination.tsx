47c1363fd2feee90c643500b08a0e9a6ed500918
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2025-11-26 16:53:21 +0600

    71-9 Adding Multi Select Filter And Other Filters For Doctor Table

--- src/components/shared/TablePagination.tsx
+++ src/components/shared/TablePagination.tsx
@@ -24,9 +24,9 @@ const TablePagination = ({ currentPage, totalPages }: TablePaginationProps) => {
     });
   };
 
-  if (totalPages <= 1) {
-    return null;
-  }
+  // if (totalPages <= 1) {
+  //   return null;
+  // }
 
   return (
     <div className="flex items-center justify-center gap-2">
