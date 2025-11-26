12c27e20a8e324e952310a0fd31f938e96c91bf9
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2025-11-26 15:38:45 +0600

    71-8 Fixing The Update Doctor Functionality

--- src/types/doctor.interface.ts
+++ src/types/doctor.interface.ts
@@ -14,6 +14,7 @@ export interface IDoctor {
   designation: string;
   specialties?: string[];
   profilePhoto?: File | string;
+  removeSpecialties?: string[];
   isDeleted?: boolean;
   averageRating?: number;
   createdAt?: string;
